3ad98db26f3950a63b2d17135ec8ca9129abb4bd
ReidDye <40401995+reid23@users.noreply.github.com>
2022-12-07 07:21:55 -0800

    working and commented, but uses DFS, not BFS. Works enough for testing though.

--- Board.py
+++ Board.py
@@ -2,22 +2,45 @@ import numpy as np
 from scipy.signal import convolve
 class Board:
     def __init__(self):
-        self.mines = np.array([1]*10 + [0]*54)
-        np.random.shuffle(self.mines)
-        self.mines = self.mines.reshape((8,8))
-        self.clues = convolve(self.mines, np.array([[1,1,1],[1,0,1],[1,1,1]]), 'same')
-        self.visible = np.zeros((8, 8), dtype=int)
+        """Creates a new Board object.
+        """
+        self.mines = np.array([1]*10 + [0]*54) #start with flat array of 1s and 0s representing mines
+        np.random.shuffle(self.mines) #shuffle it for random mine locations
+        self.mines = self.mines.reshape((8,8)) #turn it into a non-flat array (8x8 bc that's our board size)
+        self.clues = convolve(self.mines, np.array([[1,1,1],[1,0,1],[1,1,1]]), 'same') # convolution sums up all the neighbors, so each square in clues is the number of neighbors which are mines.
+        self.visible = np.zeros((8, 8), dtype=int) # like a boolean mask. This dictates which squares are visible.
+    def move(self, row, col):
+        """enters a move at row, col of the grid.
+
+        Args:
+            row (int): the row (down from the top, starting at zero) which the player chose
+            col (int): the column (from left, starting at zero)
+
+        Returns:
+            bool | np.array: either a bool or numpy array. Bool if the game is over, True if won, False if lost. Otherwise ndarray describing what should be drawn.
+        """
+        if sum(sum(self.visible)) == 54: return True #if all non-mines are visible, the player has won
+        if self.mines[row, col]==1: return False #if square clicked is a mine, the player has lost
+        self._DFS(row, col) #otherwise, do the whole reveal thing, showing all the squares
+        return self.getBoard() #then return the board in case it's useful.
     def getBoard(self):
-        return self.clues*self.visible
-    def getMoveResults(self, row, col):
-        if sum(sum(self.visible)) == 54: return True
-        if self.mines[row, col]==1: return False
-        self._BFS(row, col)
-        return self.getBoard()
-    def _BFS(self, row, col):
-        self.visible[row, col] = 1
-        if self.clues[row, col]>0: return
-        neighbors = np.array([
+        """returns a 8x8 array describing the current board state. Squares' values are the number on them. If the value is -1, the square is not visible yet.
+
+        Returns:
+            np.ndarray: 8x8 array describing the current board state. -1 means not visible, everything else is just the number the player sees.
+        """
+        return (self.clues+1)*self.visible - 1
+    def _DFS(self, row, col):
+        """recursive Depth-First Search implementation to show clues
+        yes, it's supposed to be BFS. I'll change it, but this works fine for testing.
+
+        Args:
+            row (int): the row of the move, with 0 as top and 7 as bottom
+            col (int): the column of the move, with 0 as left and 7 as right
+        """
+        self.visible[row, col] = 1 #set this node to visible
+        if self.clues[row, col]>0: return #check if any neighbors have mines, return if they do, since we can't continue searching here
+        neighbors = np.array([ #get all neighbors
             [row+1, col-1],
             [row+1, col],
             [row+1, col+1],
@@ -28,11 +51,11 @@ class Board:
             [row-1, col+1],
         ])
         for i in neighbors:
-            if len(i[i<0]) + len(i[i>7]) != 0: continue
-            if self.clues[i[0], i[1]]==0 and self.visible[i[0], i[1]]==0:
-                self.visible[i[0], i[1]]=1
-                self._BFS(*i)
-            elif self.visible[i[0], i[1]]==0:
+            if len(i[i<0]) + len(i[i>7]) != 0: continue # check if neighbor is in bounds
+            if self.clues[i[0], i[1]]==0 and self.visible[i[0], i[1]]==0: # check if square has no adjacent mines andd is not yet visible
+                self.visible[i[0], i[1]]=1 #set it to visible, and recurse to search further
+                self._DFS(*i)
+            elif self.visible[i[0], i[1]]==0: # if it does have adjacent mines, just set it to visible and don't recurse further.
                 self.visible[i[0], i[1]]=1
         
 
@@ -42,7 +65,7 @@ if __name__=='__main__':
         print(b.getBoard())
         row, col = input('move: ').split(',')
         print(row, col)
-        results = b.getMoveResults(int(row), int(col))
+        results = b.move(int(row), int(col))
         if isinstance(results, bool): break
     print('game over')
     print(b.mines)
